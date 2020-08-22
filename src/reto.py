f40617eb5a31e4abc91e55c598cf30d07a648070
CGUERRA96 <christian.guerra101013@gmail.com>
2020-08-22 11:58:17 -0500

    Segundo reto CumplidoC:/Users/51979/AppData/Local/Programs/Python/Python37/python.exe

--- src/reto.py
+++ src/reto.py
@@ -1,14 +1,29 @@
-# Reto 1 : nivel - Basico
+# Reto 2 : nivel - Medio
 try:
+    alumnos=[]
     lista_notas=[]
-    for x in range(5):
-        notas=int(input("Ingrese las notas:"))
-        lista_notas.append(notas)
-    
+    for a in range(3):
+        nuevo_alumno=input('Ingrese nuevo alumno: ')
+        alumnos.append(nuevo_alumno)
+        print(f'######## Alumno: {alumnos} ########')
+        # Reto 1 : nivel - Basico
+        for x in range(5):
+            notas=int(input("Ingrese las notas:"))
+            if  0 <= notas <= 20:
+                print('Nota correcta')
+            else:
+                print('Ingresar una nota entre 00 al 20')
+            lista_notas.append(notas)
+        lista_notas.sort()
+        alumnos_tupla=tuple(alumnos)
+        print(f'######## Sus notas son: ########')
+        print(f'La mayor nota es: {lista_notas[-1]}')
+        print(f'La menor nota es: {lista_notas[0]}')
+        print(f'El promedio es: {sum(lista_notas)/len(lista_notas)}')
+    print(type(alumnos_tupla))
 except Exception as e:
     print( "La entrada es incorrecta: escribe un numero entero")
 
-lista_notas.sort()
-print(f'La mayor nota es: {lista_notas[-1]}')
-print(f'La menor nota es: {lista_notas[0]}')
-print(f'El promedio es: {sum(lista_notas)/len(lista_notas)}')
+
+
+
