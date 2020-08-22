8b1796a659219ffbd4daa1264b0bdb6bb828ff54
CGUERRA96 <christian.guerra101013@gmail.com>
2020-08-22 14:46:21 -0500

    Tercer reto cumplido mas fino

--- src/reto.py
+++ src/reto.py
@@ -1,4 +1,4 @@
-
+ 
 try:
     # Reto 3 : nivel - (Medio-Avanzado)
     cant_alumnos= int(input("Ingrese cantidad de alumnos: "))
@@ -14,26 +14,28 @@ try:
         for x in range(cant_notas):
             print(f'Ingrese la nota {x + 1}')
             notas=int(input("Ingrese las notas:"))
-            if  0 <= notas <= 20:
-                print('Nota correcta')
-            else:
-                print('Ingresar una nota entre 00 al 20')
-                notas=int(input("Ingrese la nota correcta: "))
-            lista_notas.append(notas)
+            for i in range(0,21,1):
+                if  notas >= 0 and notas <= 20:
+                    print('Nota correcta')
+                    break
+                else:
+                    print('Ingresar una nota entre 00 al 20')
+                    notas=int(input("Ingrese la nota correcta: "))
+                    lista_notas.append(notas)
         lista_notas.sort()
         alumnos_tupla=tuple(alumnos)
-        print(f'######## Sus notas son: ########')
-        print(f'La mayor nota es: {lista_notas[-1]}')
-        print(f'La menor nota es: {lista_notas[0]}')
-        print(f'El promedio es: {sum(lista_notas)/len(lista_notas)}')
-        #conversion = dict.fromkeys(alumnos_tupla,lista_notas[x])
-    print(alumnos_tupla)
-    print(lista_notas)
-    #print(conversion)
-    print(type(alumnos_tupla))
-except Exception as e:
-    print( "La entrada es incorrecta: escribe un numero entero")
 
+except KeyboardInterrupt  as e:
+    print( "Se cerro el aplicativo")
 
+print(f'######## Sus notas son: ########')
+print(f'La mayor nota es: {lista_notas[-1]}')
+print(f'La menor nota es: {lista_notas[0]}')
+print(f'El promedio es: {sum(lista_notas)/len(lista_notas)}')
+#conversion = dict.fromkeys(alumnos_tupla,lista_notas[x])
+print(alumnos_tupla)
+print(lista_notas)
+#print(conversion)
+print(type(alumnos_tupla))
 
 
