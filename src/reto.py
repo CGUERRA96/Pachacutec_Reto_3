3b7c83b896c5ee38146fc636bfe758e6884efe06
CGUERRA96 <christian.guerra101013@gmail.com>
2020-08-22 12:25:26 -0500

    Segundo Reto mas fino

--- src/reto.py
+++ src/reto.py
@@ -5,14 +5,16 @@ try:
     for a in range(3):
         nuevo_alumno=input('Ingrese nuevo alumno: ')
         alumnos.append(nuevo_alumno)
-        print(f'######## Alumno: {alumnos} ########')
+        print(f'######## Alumno: {alumnos[a]} ########')
         # Reto 1 : nivel - Basico
         for x in range(5):
+            print(f'Ingrese la nota {x + 1}')
             notas=int(input("Ingrese las notas:"))
             if  0 <= notas <= 20:
                 print('Nota correcta')
             else:
                 print('Ingresar una nota entre 00 al 20')
+                notas=int(input("Ingrese la nota correcta: "))
             lista_notas.append(notas)
         lista_notas.sort()
         alumnos_tupla=tuple(alumnos)
