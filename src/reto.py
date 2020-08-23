eee8ae4dcaf96e561d026e3f6fc4bccd344aa072
CGUERRA96 <christian.guerra101013@gmail.com>
2020-08-22 23:24:42 -0500

    Tercer reto corregido

--- src/reto.py
+++ src/reto.py
@@ -1,4 +1,3 @@
- 
 try:
     # Reto 3 : nivel - (Medio-Avanzado)
     cant_alumnos= int(input("Ingrese cantidad de alumnos: "))
@@ -12,30 +11,29 @@ try:
         print(f'######## Alumno: {alumnos[a]} ########')
         # Reto 1 : nivel - Basico
         for x in range(cant_notas):
-            print(f'Ingrese la nota {x + 1}')
-            notas=int(input("Ingrese las notas:"))
-            for i in range(0,21,1):
+            while True:
+                print(f'Ingrese la nota {x + 1}')
+                notas=int(input("Ingrese las notas:"))
                 if  notas >= 0 and notas <= 20:
                     print('Nota correcta')
+                    lista_notas.append(notas)
                     break
                 else:
                     print('Ingresar una nota entre 00 al 20')
-                    notas=int(input("Ingrese la nota correcta: "))
-                    lista_notas.append(notas)
+                    notas=int(input("Ingrese la nota correcta: "))  
         lista_notas.sort()
         alumnos_tupla=tuple(alumnos)
+        print(f'######## Sus notas son: ########')
+        print(f'La mayor de {alumnos[a]} nota es: {lista_notas[-1]}')
+        print(f'La menor de {alumnos[a]} nota es: {lista_notas[0]}')
+        print(f'El promedio de {alumnos[a]} es: {sum(lista_notas)/len(lista_notas)}')
 
-except KeyboardInterrupt  as e:
-    print( "Se cerro el aplicativo")
-
-print(f'######## Sus notas son: ########')
-print(f'La mayor nota es: {lista_notas[-1]}')
-print(f'La menor nota es: {lista_notas[0]}')
-print(f'El promedio es: {sum(lista_notas)/len(lista_notas)}')
+except KeyboardInterrupt as t:
+    print("\nSe cerro la aplicacion")
 #conversion = dict.fromkeys(alumnos_tupla,lista_notas[x])
-print(alumnos_tupla)
+'''print(alumnos_tupla)
 print(lista_notas)
 #print(conversion)
 print(type(alumnos_tupla))
-
+'''
 
